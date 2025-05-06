cffcd042876fa173385b5c67328c702e0a8699b7
Waleed Ahmad <137788903+Waleed-Ahmad-dev@users.noreply.github.com>
2025-05-06 16:00:58 +0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a placeholder for the main function.
+	// You can add your code here to run the application.
+	fmt.Println("Hello, World!")
+	var a int = 5
+	var b int = 10
+	var sum int = a + b
+	fmt.Println("The sum of", a, "and", b, "is:", sum)
+}
